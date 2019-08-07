2f173483625f0369a1bef1fdda1082f335ebab3e
Stackingrule <messageswitching@gmail.com>
2019-08-07 09:42:33 +0800

    finished red-black-tree

--- Red-Black-Tree/RBTree.h
+++ Red-Black-Tree/RBTree.h
@@ -22,7 +22,7 @@ private:
 			color = RED;
 		}
 
-		RBTree(RBNode* node) {
+		RBNode(RBNode* node) {
 			this->key = node->key;
 			this->value = node->value;
 			this->left = node->left;
@@ -34,12 +34,12 @@ private:
 	RBNode* root;
 	int _size;
 
-	//     node                     x
-	//    /    \       左旋转      /  \
-	//    T1    x   ---------->  node  T3
-	//         / \              /   \
-	//        T2 T3            T1   T2
-	//        
+	/*     node                     x          */
+	/*    /    \       左旋转      /  \        */
+	/*    T1    x   ---------->  node  T3      */
+	/*         / \              /   \          */
+	/*        T2 T3            T1   T2         */
+	/*                                         */        
 	RBNode* leftRotate(RBNode* node) {       
 		RBNode* x = node->right;	    	 
 		node->right = x->left;               
@@ -58,6 +58,21 @@ private:
 		return x;
 	}
 
+	RBNode* find(RBNode* node, Key key) {
+		if(node == nullptr) {
+			return nullptr;
+		}
+		if(key = node->key) {
+			return node;
+		} 
+		else if (key < node->key) {
+			return find(node->left, key);
+		}
+		else {
+			return find(node->right, key);
+		}
+	}
+
 	void flipColor(RBNode* node) {
 		node->color = RED;
 		node->left = node->right = BLACK;
@@ -66,14 +81,40 @@ private:
 	RBNode* insert(RBNode* node, Key key, Value value) {
 		if(node == nullptr) {
 			_size++;
-			return new RBNode(key, value);
+			return new RBNode(key, value);	//默认为红节点		
 		}
 
 		if(key < node->key) {
-
+			node->left = insert(node->left, key, value);
 		}
+		else if(key > node->key) {
+			node->right = insert(node->right, key, value);
+		}
+		else {
+			node->value = value;
+		}
+
+		if(isRed(node->right) && !isRed(node->left)) {
+			node = leftRotate(node);
+		}
+		if(isRed(node->left) && isRed(node->left->left)) {
+			node = rightRotate(node);
+		}
+		if(isRed(node->left) && isRed(node->right)) {
+			flipColor(node);
+		}
+
+		return node;
 	}
 
+	void destroy(RBNode* node) {
+		if(node != nullptr) {
+			destroy(node->left);
+			destroy(node->right);
+			delete node;
+			_size--;
+		}
+	}
 
 
 public:
@@ -94,18 +135,32 @@ public:
 		return _size == 0;
 	}
 
+	bool isRed(RBNode* node) {
+		if(node == nullptr)
+			return BLACK;
+		return node->color;
+	}
+
 	void insert(Key key, Value value) {
 		insert(root, key, value);
 		root->color = BLACK;
 	}
 
 	bool contain(Key key) {
-		return contain(root, key);
+		return find(root, key) != nullptr;
 	}
 
+	Value* getValue(Key key) {
+		RBNode* node = find(root, key);
+		return node == nullptr ? nullptr : &(node->value);
+	}
 
-
-
+	void set(Key key, Value value) {
+		RBNode* node = find(root, key);
+		if(node != nullptr) {
+			node->value = value;
+		}
+	}
 	
 };
 
